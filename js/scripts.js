b6cf9a116c7f76cf18d9ddd3988cb8e33147d6c1
Adela <adela.yohana@gmail.com>
2020-01-15 17:33:26 -0800

    Complete forms and confirmation receipt modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
+  $("form#orderForm").submit(function(event) {
+    var outputName = $("input#fullName").val();
+    $("#outputName").text(outputName);
+
+    var shoppingItem = $("#shoppingItem").val();
+    var quantity = $("#quantity").val();
+    $("#outputItems").text(quantity + " " + shoppingItem);
+
+    var street = $("input#address").val();
+    var city = $("input#city").val();
+    var state = $("input#state").val();
+    var zip = $("input#zip").val();
+    var outputAddress = street + ", " + city + ", " + state + " " + zip;
+    $("#outputAddress").text(outputAddress);
+
+    event.preventDefault();
+  });
 
 });
